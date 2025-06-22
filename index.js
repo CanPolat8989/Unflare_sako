55f2d81f50daafcf916bc5761ed495df15391d80
CanPolat8989 <89canpolat89@gmail.com>
2025-06-22 17:29:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ app.post('/scrape', async (req, res) => {
     await browser.close();
 
     console.log('✅ HTML erfolgreich extrahiert.');
-    console.log(content);
     res.send(content);
   } catch (error) {
     console.error('❌ Fehler beim Scraping:', error);
